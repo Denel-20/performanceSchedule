1cf7c3cf402d6d63262641d43d5586383a629969
Denel-20 <dcsbsc02@gmail.com>
2025-03-13 23:00:21 +0900

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -45,37 +45,9 @@ function updateParentCheckbox(parentId, childrenId) {
 
 function updateSubjects() {
     const subjects = [
-        { id: 'dkorean-a', data: '2025. 3. 1 (0교시)' , name: '심화국어A', details: '내용이 지정되지 않았습니다.' },
-        { id: 'dkorean-b', data: '2025. 3. 2 (0교시)' , name: '심화국어B', details: '내용이 지정되지 않았습니다.' },
-        { id: 'dkorean-c', data: '2025. 3. 3 (0교시)' , name: '심화국어C', details: '내용이 지정되지 않았습니다.' },
-        { id: 'dkorean-d', data: '2025. 3. 4 (0교시)' , name: '심화국어D', details: '내용이 지정되지 않았습니다.' },
-        { id: 'geometry-b', data: '2025. 3. 5 (0교시)' , name: '기하B', details: '내용이 지정되지 않았습니다.' },
-        { id: 'geometry-c', data: '2025. 3. 6 (0교시)' , name: '기하C', details: '내용이 지정되지 않았습니다.' },
-        { id: 'geometry-d', data: '2025. 3. 7 (0교시)' , name: '기하D', details: '내용이 지정되지 않았습니다.' },
-        { id: 'sac-a', data: '2025. 3. 8 (0교시)' , name: '화법과 작문A', details: '내용이 지정되지 않았습니다.' },
-        { id: 'sac-b', data: '2025. 3. 9 (0교시)' , name: '화법과 작문B', details: '내용이 지정되지 않았습니다.' },
-        { id: 'sac-c', data: '2025. 3. 10 (0교시)' , name: '화법과 작문C', details: '내용이 지정되지 않았습니다.' },
-        { id: 'english-a', data: '2025. 3. 11 (0교시)' , name: '심화영어A', details: '내용이 지정되지 않았습니다.' },
-        { id: 'english-b', data: '2025. 3. 12 (0교시)' , name: '심화영어B', details: '내용이 지정되지 않았습니다.' },
-        { id: 'math-a', data: '2025. 3. 13 (0교시)' , name: '심화수학A', details: '내용이 지정되지 않았습니다.' },
-        { id: 'math-b', data: '2025. 3. 14 (0교시)' , name: '심화수학B', details: '내용이 지정되지 않았습니다.' },
-        { id: 'physics-c', data: '2025. 3. 15 (0교시)' , name: '물리2C', details: '내용이 지정되지 않았습니다.' },
-        { id: 'physics-d', data: '2025. 3. 16 (0교시)' , name: '물리2D', details: '내용이 지정되지 않았습니다.' },
-        { id: 'chemistry-a', data: '2025. 3. 17 (0교시)' , name: '화학2A', details: '내용이 지정되지 않았습니다.' },
-        { id: 'chemistry-b', data: '2025. 3. 18 (0교시)' , name: '화학2B', details: '내용이 지정되지 않았습니다.' },
-        { id: 'chemistry-c', data: '2025. 3. 19 (0교시)' , name: '화학2C', details: '내용이 지정되지 않았습니다.' },
-        { id: 'life-a', data: '2025. 3. 20 (0교시)' , name: '생명2A', details: '내용이 지정되지 않았습니다.' },
-        { id: 'life-b', data: '2025. 3. 21 (0교시)' , name: '생명2B', details: '내용이 지정되지 않았습니다.' },
-        { id: 'life-c', data: '2025. 3. 22 (0교시)' , name: '생명2C', details: '내용이 지정되지 않았습니다.' },
-        { id: 'life-d', data: '2025. 3. 23 (0교시)' , name: '생명2C', details: '내용이 지정되지 않았습니다.' },
-        { id: 'earth-b', data: '2025. 3. 24 (0교시)' , name: '지구2B', details: '내용이 지정되지 않았습니다.' },
-        { id: 'earth-d', data: '2025. 3. 25 (0교시)' , name: '지구2D', details: '내용이 지정되지 않았습니다.' },
-        { id: 'travel-ag', data: '2025. 3. 26 (0교시)' , name: '여행지리A가', details: '내용이 지정되지 않았습니다.' },
-        { id: 'travel-an', data: '2025. 3. 27 (0교시)' , name: '여행지리A나', details: '내용이 지정되지 않았습니다.' },
-        { id: 'travel-b', data: '2025. 3. 28 (0교시)' , name: '여행지리B', details: '내용이 지정되지 않았습니다.' },
-        { id: 'travel-c', data: '2025. 3. 29 (0교시)' , name: '여행지리C', details: '내용이 지정되지 않았습니다.' },
-        { id: 'science', data: '2025. 3. 30 (0교시)' , name: '과학 과제 연구', details: '내용이 지정되지 않았습니다.' },
-        { id: 'art', data: '2025. 3. 31 (0교시)' , name: '미술', details: '내용이 지정되지 않았습니다.' }
+        { id: 'art', data: '2025. 4. 4 (4교시)' , name: '미술', details: ['자신의 진로와 관련된 책 속 한 문장을 일러스트와 어울리게 자신의 서체로 표현한다.', ' *문장을 암기하여 실시한다.(문장 또는 책 수정시 반드시 리로스쿨 수정정)'] },
+        { id: 'art', data: '2025. 4. 11 (4교시)' , name: '미술', details: ['자신의 진로와 관련된 책 속 한 문장을 일러스트와 어울리게 자신의 서체로 표현한다.', ' *문장을 암기하여 실시한다.(문장 또는 책 수정시 반드시 리로스쿨 수정정)'] },
+        { id: 'art', data: '2025. 4. 18 (4교시)' , name: '미술', details: ['캘리그래피 발표대본을 자유 형식으로 200자 원고 작성하기', ' *책제목, 저자, 선택 문구 정확히 소개']}
     ];
 
     const subjectContainer = document.getElementById('selected-subjects');
@@ -105,7 +77,7 @@ function updateSubjects() {
         } else if (subjectDate > tomorrow && subjectDate <= weekLater) {
             subjectBar.style.backgroundColor = '#FFFF8C';
         }  else if (subjectDate > weekLater) {
-            subjectBar.style.backgroundColor = '#C0FFFF';
+            subjectBar.style.backgroundColor = '#C8FAC8';
         } else if (subjectDate < today) {
             subjectBar.style.backgroundColor = '#F4F4F4';
         }
